42aa86e0d093a016e2a2cd52010cb91a40f3a423
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-04 21:27:47 +0000

    test

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+TEST        </a>
       </header>
     </div>
   );
