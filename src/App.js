7f52794adf06103475eef932afa8e7341a0abf38
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-05 09:38:13 +0000

    Added pics and captions

--- src/App.js
+++ src/App.js
@@ -5,25 +5,39 @@ import face from './face.png';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to our Startup - Meet the team.</h1>
+      <h1>Welcome to our Startup!</h1>
+      <p>We're here to disrupt, democratise and dominate a legacy industry.</p>
+      <h1>Meet the team!</h1>
       <div class="container">
         <div class="item">
           <img src={face} alt="a face"/>
+          Elon Musk
+          <br></br>CEO
         </div>
         <div class="item">
           <img src={face} alt="a face"/>
+          Bill Gates
+          <br></br>CTO
         </div>
         <div class="item">
           <img src={face} alt="a face"/>
+          Jeff Bezos
+          <br></br>COO
         </div>
         <div class="item">
           <img src={face} alt="a face"/>
+          Jack Dorsey
+          <br></br>Back End Developer
         </div>
         <div class="item">
           <img src={face} alt="a face"/>
+          Mark Zuckerberg
+          <br></br>Front End Developer
         </div>
         <div class="item">
           <img src={face} alt="a face"/>
+          Larry Page
+          <br></br>Full Stack Developer
         </div>
       </div>
     </div>
