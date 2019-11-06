193b32f9b58ee06634b259bcced7befd9ef5aabf
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-06 09:28:42 +0000

    removed hoverimg package

--- src/Face.js
+++ src/Face.js
@@ -1,16 +1,13 @@
 import face from './face.png'
 import React from 'react';
-import HoverImage from "react-hover-image";
 import bill from './bill.jpg'
 
 export default function Face(props) {
     return (
         <>
-        <HoverImage src={face} alt={props.name} hoverSrc={props.pic}/>
+        <img src={face} alt={props.name} hoverSrc={props.pic}/>
         {props.name}
         </>
     )
 
 }
-
-//Need to add conditional rendering, only changing the face and the caption (name)
